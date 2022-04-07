c07b996538c7c47af5e35b8031f2dbf42e95b6ac
EfimovSergeiV <efimovsergeiv@gmail.com>
2022-04-07 22:21:27 +0300

    add: vote

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from content.models import VoteModel, ChoiceModel
 
-# Register your models here.
+
+class InlineChoiceModel(admin.TabularInline):
+    model = ChoiceModel
+    extra = 0
+
+
+class VoteModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'start_date', 'end_date', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('title', 'description')
+    fields = ('title', 'description', 'start_date', 'end_date', 'is_active')
+    readonly_fields = ('start_date', 'end_date')
+    actions = ['activate', 'deactivate']
+    inlines = [InlineChoiceModel,]
+
+    def activate(self, request, queryset):
+        queryset.update(is_active=True)
+    activate.short_description = 'Активировать'
+
+    def deactivate(self, request, queryset):
+        queryset.update(is_active=False)
+    deactivate.short_description = 'Деактивировать'
+
+
+admin.site.register(VoteModel, VoteModelAdmin)
--- content/models.py
+++ content/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class VoteModel(models.Model):
+    """
+    Модель для голосования
+    """
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    start_date = models.DateTimeField(verbose_name='Дата начала')
+    end_date = models.DateTimeField(verbose_name='Дата окончания')
+    is_active = models.BooleanField(default=True, verbose_name='Активно')
+
+    class Meta:
+        verbose_name = 'Голосование'
+        verbose_name_plural = 'Голосования'
+
+    def __str__(self):
+        return self.title
+
+
+class ChoiceModel(models.Model):
+    """
+    Модель для вариантов ответов
+    """
+    vote = models.ForeignKey(VoteModel, on_delete=models.CASCADE, verbose_name='Голосование')
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    is_active = models.BooleanField(default=True, verbose_name='Активно')
+
+    class Meta:
+        verbose_name = 'Вариант ответа'
+        verbose_name_plural = 'Варианты ответов'
+
+    def __str__(self):
+        return self.title
